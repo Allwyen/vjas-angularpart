6877751787591d3d6255d909f9ce6a5ce5bab87e
allwyen <allwyen27@gmail.com>
2019-09-13 10:20:45 +0530

    Hosted angularpart

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -12,122 +12,122 @@ export class ApiService {
 
   vjasregister(data)
   {
-    return this.http.post("http://localhost:5566/vjasregister",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasregister",data);
   }
 
   vjaslogin(data)
   {
-    return this.http.post("http://localhost:5566/vjaslogin",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjaslogin",data);
   }
 
   vjasforgotpwd(data)
   {
-    return this.http.post("http://localhost:5566/vjasforgotpwd",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasforgotpwd",data);
   }
 
   vjasgetuser(data)
   {
-    return this.http.post("http://localhost:5566/vjasgetuser",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasgetuser",data);
   }
 
   /* Admin Section */
 
   vjasviewuser()
   {
-    return this.http.get("http://localhost:5566/vjasviewuser");
+    return this.http.get("https://vjas-nodejs.herokuapp.com/vjasviewuser");
   }
 
   vjasuserstatus(data)
   {
-    return this.http.post("http://localhost:5566/vjasuserstatus",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasuserstatus",data);
   }
 
   vjaspendingcar()
   {
-    return this.http.get("http://localhost:5566/vjaspendingcar");
+    return this.http.get("https://vjas-nodejs.herokuapp.com/vjaspendingcar");
   }
 
   vjasworkstatus()
   {
-    return this.http.get("http://localhost:5566/vjasworkstatus");
+    return this.http.get("https://vjas-nodejs.herokuapp.com/vjasworkstatus");
   }
 
   vjasworkstatussearch(data)
   {
-    return this.http.post("http://localhost:5566/vjasworkstatussearch",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasworkstatussearch",data);
   }
 
   /* Staff Section */
 
   vjasviewcar(data)
   {
-    return this.http.post("http://localhost:5566/vjasviewcar",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasviewcar",data);
   }
 
   vjasinsertcar(data)
   {
-    return this.http.post("http://localhost:5566/vjasinsertcar",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasinsertcar",data);
   }
 
   vjasupdatecar(data)
   {
-    return this.http.post("http://localhost:5566/vjasupdatecar",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasupdatecar",data);
   }
 
   vjasinsertissue(data)
   {
-    return this.http.post("http://localhost:5566/vjasinsertissue",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasinsertissue",data);
   }
 
   vjasviewmechanic()
   {
-    return this.http.get("http://localhost:5566/vjasviewmechanic");
+    return this.http.get("https://vjas-nodejs.herokuapp.com/vjasviewmechanic");
   }
 
   vjasviewcarassign(data)
   {
-    return this.http.post("http://localhost:5566/vjasviewcarassign",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasviewcarassign",data);
   }
 
   vjasinsertcarassign(data)
   {
-    return this.http.post("http://localhost:5566/vjasinsertcarassign",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasinsertcarassign",data);
   }
 
   vjasupdatejstatus(data)
   {
-    return this.http.post("http://localhost:5566/vjasupdatejstatus",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasupdatejstatus",data);
   }
 
   /* Mechanic Section */
 
   vjasviewmechanictask(data)
   {
-    return this.http.post("http://localhost:5566/vjasviewmechanictask",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasviewmechanictask",data);
   }
 
   vjasupdateastatus(data)
   {
-    return this.http.post("http://localhost:5566/vjasupdateastatus",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasupdateastatus",data);
   }
 
   vjasrevokejstatus(data)
   {
-    return this.http.post("http://localhost:5566/vjasrevokejstatus",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasrevokejstatus",data);
   }
 
   vjascownersendemail(data)
   {
-    return this.http.post("http://localhost:5566/vjascownersendemail",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjascownersendemail",data);
   }
 
   vjasmechcompletedtask(data)
   {
-    return this.http.post("http://localhost:5566/vjasmechcompletedtask",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasmechcompletedtask",data);
   }
 
   vjasmechcompletedtasksearch(data)
   {
-    return this.http.post("http://localhost:5566/vjasmechcompletedtasksearch",data);
+    return this.http.post("https://vjas-nodejs.herokuapp.com/vjasmechcompletedtasksearch",data);
   }
 }
